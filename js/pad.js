92434ec7fe04c056a19975d7e5dbcbaa0f40cbe3
Cristian García <cristian99garcia@gmail.com>
2018-02-10 17:36:14 -0300

    Agregar 'getCaretPosition' y 'setCaretPosition' a la entrada de texto

--- js/pad.js
+++ js/pad.js
@@ -16,7 +16,7 @@ Entry.prototype.setFocus = function(focused) {
 
   if (this.focused) {
     this.entry.focus();
-    console.log(this.entry);
+    //console.log(this.entry);
   }
 }
 
@@ -40,11 +40,14 @@ Entry.prototype.getText = function() {
   return this.entry[0].textContent;
 }
 
-Entry.prototype.insertExpEntry = function() {
+Entry.prototype.insertExpEntryAtCursor = function() {
   var id = "entry-" + entriesCount;
   var style = "font-size: 15px;" +
               "position: relative;" +
-              "top: 2px;";
+              "top: -40px;" +
+              "min-width: 8px;" +
+              "width: 8px;" +
+              "margin-left: 20px;";
 
   var inner = "<sup><div id='" + id + "' contenteditable='true' style='" + style + "' class='exp-entry'>a</div></sup>";
   this.entry[0].innerHTML += inner;
@@ -58,18 +61,56 @@ Entry.prototype.insertExpEntry = function() {
   entriesCount += 1;
 }
 
-Entry.prototype.insertAtCursor = function(insertText) {
-  /*
+Entry.prototype.getCaretPosition = function() {
+  var caretPos = 0;
+  var sel, range;
+
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+      if (range.commonAncestorContainer.parentNode == this.entry[0]) {
+        caretPos = range.endOffset;
+      }
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    if (range.parentElement() == this.entry[0]) {
+      var tempEl = document.createElement("span");
+      this.entry[0].insertBefore(tempEl, this.entry[0].firstChild);
+      var tempRange = range.duplicate();
+      tempRange.moveToElementText(tempEl);
+      tempRange.setEndPoint("EndToEnd", range);
+      caretPos = tempRange.text.length;
+    }
+  }
+
+  return caretPos;
+}
+
+Entry.prototype.setCaretPosition = function(caretPos) {
+  var range = document.createRange();
+  var sel = window.getSelection();
+  range.setStart(this.entry[0].childNodes[0], caretPos);
+  range.collapse(true);
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
+
+Entry.prototype.insertTextAtCursor = function(insertText) {
   var text = this.getText();
-  var newText = text.substring(0, this.cursorPosition) + insertText + text.substring(this.cursorPosition, text.length + 1);
-  console.log(newText);
-  this.cursorPosition += insertText.length;
+  var cursorPosition = this.getCaretPosition();
+  var newText = text.substring(0, cursorPosition) + insertText + text.substring(cursorPosition, text.length + 1);
+
   this.setText(newText);
-  */
+  this.setCaretPosition(cursorPosition + insertText.length);
 }
 
 Entry.prototype.init = function() {
   var _this = this;
+
+  var ignore = ["Alt", "Control", "Delete", "Backspace", "Tab", "Shift", "AltGraph", "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowRight", "Space"];
+
   $(this.entry).on("focusin", function(event) {
     _this.setFocus(true);
   });
@@ -79,15 +120,18 @@ Entry.prototype.init = function() {
   });
 
   $(this.entry).on("keydown", function(event) {
+    console.log(event.srcElement);
     if (event.key === "Enter") {
       return false;
     } else if (event.originalEvent.code === "Quote") {
       if (_this.hasFocus()) {
-        _this.insertExpEntry();
+        _this.insertExpEntryAtCursor();
         return false;
       } else {
       }
-    } else {
+    } else if (!ignore.includes(event.key) && !ignore.includes(event.originalEvent.code) && !event.altKey && !event.ctrlKey) {
+      _this.insertTextAtCursor(event.key);
+      return false;
     }
   });
 }
